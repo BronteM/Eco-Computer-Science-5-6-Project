01432315f723be32ef2ec9e661b211284226f8e7
B3anss <61066242+B3anss@users.noreply.github.com>
2020-05-02 22:23:17 -0700

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,29 +1,76 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.lightBlue[200],
-        appBar: AppBar(
-          title: Text('Eco Comp Sci Project'),
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: Center(
-          child: Column(
+void main() => runApp(MaterialApp(
+  theme:
+  ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+  debugShowCheckedModeBanner: false,
+  home: SplashScreen(),
+));
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.greenAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Text('Hello'),
-              FlatButton(
-                onPressed: () {},
-                child: Image(
-                  image: NetworkImage(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThmOI_XEZmumkJhU4ey8aozTSMWBUQceCzWYfHCzijGbFoDmy8&usqp=CAU'),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.supervised_user_circle,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Recycle",style: TextStyle(color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
                 ),
               ),
+              Expanded(flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text("Reduce\nReuse\nRecycle",
+                        style: TextStyle(
+                            color:Colors.white,
+                            fontSize:18,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              )
             ],
-          ),
-        ),
+          )
+        ],
       ),
-    ),
-  );
+    );
+  }
 }
+
